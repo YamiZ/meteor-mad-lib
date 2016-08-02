4e49a872e4bcc31f51ba26ab727e71c6388bd1d8
Ivan M <Ivan M>
2016-08-01 20:24:47 -0400

    Logged inputs

--- client/main.js
+++ client/main.js
@@ -1,8 +1,26 @@
 Template.body.events({
-
-	'click': function(){
+	'submit form': function(event, template){
 		event.preventDefault();
-		console.log("hello,world");
-	}
+		let exclamation = template.find('#exclamation').value;
+		let adverb = template.find('#adverb').value;
+		let noun = template.find('#noun').value;
+		let verb = template.find('#verb').value;
 
+		console.log(exclamation + adverb + noun + verb);
+	}
 });
+
+/*Template.body.helpers({
+	exclamation: function(){
+		return Session.get('exclamation')
+	},
+	adverb: function(){
+		return Session.get('adverb')
+	},
+	noun: function(){
+		return Session.get('noun')
+	},
+	verb: function(){
+		return Session.get('verb')
+	},
+});*/
