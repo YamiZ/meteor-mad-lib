77877c64433f37a62a0fe083e8279b423101c2b1
yamiel zea <yamielzea@yamiels-MacBook-Pro.local>
2016-08-01 20:47:43 -0400

    added jQuery

--- client/main.js
+++ client/main.js
@@ -1,12 +1,17 @@
 Template.body.events({
 	'submit form': function(event, template){
 		event.preventDefault();
+
 		let exclamation = template.find('#exclamation').value;
 		let adverb = template.find('#adverb').value;
 		let noun = template.find('#noun').value;
 		let verb = template.find('#verb').value;
 
-		console.log(exclamation + adverb + noun + verb);
+		let madLib = exclamation + "you say"+ adverb+" "+noun+" "+verb;
+
+		$("#createMadLib").hide();
+		$("#createMadLib").text(madLib).fadeIn("slow").css("color","#0000ff");
+		
 	}
 });
 
